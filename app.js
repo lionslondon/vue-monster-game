1de56e2ce72e4e12d499b91fd90083055cc54765
MOESAGE\Sage <mohammad.sage@gmail.com>
2017-12-07 13:39:42 +0800

    Log has been added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data:{
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns:[]
     },
     methods:{
         startGame: function(){
@@ -12,9 +13,12 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function(){
-
-            this.monsterHealth -= this.calculateDamage(3, 10);
-
+            var damage =  this.calculateDamage(3, 10);
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for :' + damage
+            });
             if(this.checkWin()){
                 return;
             }
@@ -43,8 +47,14 @@ new Vue({
             this.gameIsRunning = false;
         },
         monsterAttacks: function(){
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+           
+            this.turns.unshift({
+                isPlayer: false,
+                text:'Monster hits Player for :' + damage
+            });
 
-            this.playerHealth -= this.calculateDamage(5, 12);
             this.checkWin();
         }, 
         calculateDamage: function(min, max){
